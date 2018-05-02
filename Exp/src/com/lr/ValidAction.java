e1c7e4860a6ab05854cee06c05ed24232054ae1f
unknown <kennycai2836@163.com>
2018-05-02 23:16:29 +0800

    Exp3

--- Exp/src/com/lr/ValidAction.java
+++ Exp/src/com/lr/ValidAction.java
@@ -5,6 +5,8 @@ import com.opensymphony.xwork2.ActionSupport;
 public class ValidAction extends ActionSupport{
 	private String username;
 	private String password;
+	private String email;
+
 	public String getUsername() {
 		return username;
 	}
@@ -16,6 +18,12 @@ public class ValidAction extends ActionSupport{
 	}
 	public void setPassword(String password) {
 		this.password = password;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
 	}
 	@Override
 	public void validate() {
